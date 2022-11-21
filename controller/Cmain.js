510c5ed61c24fb4c9dd7bb4a6919df082b127d40
Rukieup <jominhee1221@gmail.com>
2022-11-21 14:11:21 +0900

    회원가입 서버랑 연결

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -4,15 +4,34 @@ exports.main = (req, res) => {
   res.render("main");
 };
 
-
 exports.login = (req, res) => {
   res.render("login");
 };
-exports.getGroupCreate =  (req, res) => {
-  res.render("groupCreate")
-};
+
+// exports.getGroupCreate =  (req, res) => {
+//   res.render("groupCreate")
+// };
 
 exports.getRegister =  (req, res) => {
   res.render("register")
 };
 
+exports.postProfileImg = (req, res) => {
+    // uploadDetail.single('dynamicFile');
+    res.send(req.file);
+}
+
+exports.postSignup = (req, res) => {
+  console.log('postSignup', req.body);
+  models.MUser.create({
+    profile: req.body.profile,
+    userid: req.body.userid,
+    pw: req.body.pw,
+    name: req.body.name,
+    nickname: req.body.nickname,
+    address: req.body.address,
+  }).then((result)=> {
+    console.log('create >> ', result);
+    res.send(result);
+  });
+}
