016bcda58f57bdc6c8b4409bd8ab7d29a8ce8370
Rukieup <jominhee1221@gmail.com>
2022-11-25 16:52:55 +0900

    메인 css 수정

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,28 @@
-// const cards = document.querySelector(".cards");
-// const cardArticle = document.querySelector(".cardArticle");
-// const card = document.querySelectorAll(".card");
-// const currentIdx = 0;
-// const controls = document.querySelectorAll(".controls");
-// const buttonLeft = document.querySelector(".left");
-// const buttonRight = document.querySelector(".right");
-
-// buttonLeft.addEventListener("click", () => {
-//   currentIndex--;
-//   currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
-//   cardArticle.style.marginLeft = `-${cards.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
-// });
-
-// buttonRight.addEventListener("click", () => {
-//   currentIndex++;
-//   currentIndex = currentIndex >= card.length ? card.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
-//   cardArticle.style.marginLeft = `-${cards.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
-// });
-
 let scrollLeft = document.querySelector('.prev')
 let scrollRight = document.querySelector('.next')
 let cardArticle = document.querySelector('.cardArticle');
+let serchInput = document.querySelector('.serch-input')
 
 scrollLeft.onclick = () => {
-  cardArticle.scrollLeft = 0;
+  cardArticle.scrollLeft -= 1020;
 };
 
 scrollRight.onclick = () => {
-  cardArticle.scrollLeft = cardArticle.scrollWidth;
+  cardArticle.scrollLeft += 1020;
 };
+
+// function serchGroup() {
+//   axios({
+//     method: 'POST',
+//     url: '/serch',
+//     data: {
+//       serchValue : serchInput.value,
+//     }
+//   })
+
+//   .then((res => {
+//     console.log(res.result)
+//     // return res.data;
+//   })
+//   )
+// }
